0f26541f00a550d748d35059280dfc839c2a287b
Jordon de Hoog <jordon.dehoog@gmail.com>
2017-10-25 19:37:45 -0400

    Testing updated site on gh pages

--- react-static-routes.js
+++ react-static-routes.js
@@ -1,18 +1,15 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 
-    import React, { Component } from 'react'
-    import { Switch, Route } from 'react-router-dom'
+import src_containers_Home from "../src/containers/Home";
 
-    import src_containers_Home from '../src/containers/Home'
-import src_containers____ from '../src/containers/404'
-
-    export default class Routes extends Component {
-      render () {
-        return (
-          <Switch>
-              <Route exact path={'/'} component={src_containers_Home} />
-              <Route component={src_containers____} />
-          </Switch>
-        )
-      }
-    }
-  
+export default class Routes extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path={"/"} component={src_containers_Home} />
+        <Route component={src_containers_Home} />
+      </Switch>
+    );
+  }
+}
