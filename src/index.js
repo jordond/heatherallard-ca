8263c3641261bb4b83065df72ca7bc8877c0acf0
jordond <jordon.dehoog@ek3.com>
2018-01-08 11:45:18 -0500

    Move and consolidate some files
    App has been fused with index.js, to create a cleaner file structure.
    I'm sure components shouldn't be mixed with root logic, but oh well.
    
    Add Typist for typing some text

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./app";
-import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from "./utils/registerServiceWorker";
+
+// Components
+import Nav from "./nav";
+import Home from "./sections/home";
+import Features from "./sections/features";
+
+import "./app.css";
+
+const links = ["about", "work", "contact"];
+
+const App = () => (
+  <div>
+    <Nav links={links.map(text => ({ dest: `/#${text}`, text }))} />
+    <Home />
+    <Features />
+  </div>
+);
+
+export default App;
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
 registerServiceWorker();
